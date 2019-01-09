5b835bd10d7c03078fe791a6c654269424616592
Adamantios <manoszaras94@gmail.com>
2019-01-09 19:44:51 +0200

    Add genes.

--- definitions.py
+++ definitions.py
@@ -23,6 +23,15 @@ DATASET_MNIST = 'mnist.csv'
 # The robot wall following dataset's filename.
 DATASET_WALL_FOLLOWING = 'wall_following.csv'
 
+# The genes data filename.
+DATA_GENES = 'genes.csv'
+# The genes labels filename.
+LABELS_GENES = 'genes_labels.csv'
+# The genes train dataset's filename.
+DATASET_GENES_TRAIN = 'genes_train.csv'
+# The genes test dataset's filename.
+DATASET_GENES_TEST = 'genes_test.csv'
+
 """ ----------------------------------------------------------------- """
 
 """ ------------------------- Do not modify. -------------------------"""
@@ -40,6 +49,15 @@ __MNIST_PATH = join(__ROOT, DATASETS, DATASET_MNIST)
 
 # The path to the seizure detection train dataset.
 __WALL_FOLLOWING_PATH = join(__ROOT, DATASETS, DATASET_WALL_FOLLOWING)
+
+# The path to the genes data.
+__GENES_DATA_PATH = join(__ROOT, DATASETS, DATA_GENES)
+# The path to the genes labels.
+__GENES_LABELS_PATH = join(__ROOT, DATASETS, LABELS_GENES)
+# The path to the genes train dataset.
+__GENES_TRAIN_PATH = join(__ROOT, DATASETS, DATASET_GENES_TRAIN)
+# The path to the genes test dataset.
+__GENES_TEST_PATH = join(__ROOT, DATASETS, DATASET_GENES_TEST)
 
 # Check plotting mode.
 if PLOTTING_MODE != 'show' and PLOTTING_MODE != 'save' and PLOTTING_MODE != 'both':
