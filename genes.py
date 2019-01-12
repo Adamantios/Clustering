d3455588ebd727408c63a0be2c6fb3b4aff156a3
Adamantios <manoszaras94@gmail.com>
2019-01-12 14:54:15 +0200

    Were using new line on the filename.

--- genes.py
+++ genes.py
@@ -161,7 +161,7 @@ def assign_to_clusters(x_train: np.ndarray, clusters: np.ndarray, x_test: np.nda
     if PLOTTING_MODE != 'none':
         # Plot data vs clusters.
         plotter.subfolder = 'classification'
-        plotter.filename = 'data_vs_clusters\nk={}'.format(clf_params['n_neighbors'])
+        plotter.filename = 'data_vs_clusters-k={}'.format(clf_params['n_neighbors'])
         plotter.xlabel = 'first feature'
         plotter.ylabel = 'second feature'
         plotter.title = 'Classified data vs Clusters'
